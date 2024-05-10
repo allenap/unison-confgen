d074ccb8bfbcec5563128a845f58e6d52289ddfd
Gavin Panella <gavinpanella@gmail.com>
2024-05-10 11:01:44 +0200

    Fix .prf file naming
    Improve a lot of the code too.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use std::collections::{HashMap, HashSet};
-use std::io::Write;
+use std::fmt::Write;
+use std::io::Write as WriteFmt;
 use std::path::PathBuf;
 
+use itertools::chain;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
+#[serde(deny_unknown_fields)]
 struct Config {
     hosts: HashMap<String, Host>,
 }
@@ -26,31 +29,30 @@ fn main() -> Result<()> {
     // Get the hostname of the machine we're running on.
     let hostname = hostname::get()?.into_string().unwrap_or_default();
     // Trim the domain name from the hostname.
-    let hostname = hostname
+    let here_name = hostname
         .split_once('.')
         .map_or(hostname.as_str(), |(part, _)| part);
 
     // Load the schemes we can sync.
-    let mut config: Config = {
+    let mut hosts = {
         let stdin = std::io::stdin();
-        serde_yaml::from_reader(stdin)?
+        serde_yaml::from_reader::<_, Config>(stdin)?.hosts
     };
 
-    let here_config = config
-        .hosts
-        .remove(hostname)
-        .ok_or(format!("Configuration for {:?} was not found.", hostname))?;
-    let there_configs = config.hosts;
+    let here_config = hosts
+        .remove(here_name)
+        .ok_or(format!("Configuration for {here_name:?} was not found."))?;
+    let there_configs = hosts;
 
-    let peer_configs = {
-        let mut configs = peer_configs(&here_config, &there_configs).collect::<Vec<_>>();
+    let pair_configs = {
+        let mut configs = pair_configs(&here_config, &there_configs).collect::<Vec<_>>();
         configs.sort_by_key(|(name, _)| name.as_str());
         configs
     };
 
     let mut key: u8 = 1;
-    for (_there_name, there_config) in peer_configs {
-        let peer_sets = {
+    for (there_name, there_config) in pair_configs {
+        let pair_sets = {
             let mut sets = here_config
                 .sets
                 .intersection(&there_config.sets)
@@ -60,19 +62,23 @@ fn main() -> Result<()> {
             sets
         };
 
-        let (name, mut lines) = make_config(&here_config, there_config, &peer_sets)?;
+        let mut config = config(
+            (here_name, &here_config),
+            (there_name, there_config),
+            &pair_sets,
+        )?;
 
         if (1..=9).contains(&key) {
-            lines.push("".to_string());
-            lines.push(format!("key = {key}"));
+            writeln!(config)?;
+            writeln!(config, "key = {key}")?;
             key += 1;
         }
 
-        let path = PathBuf::from(format!("{}.prf", name));
-        let mut file = std::fs::File::create(&path)?;
+        let prf_path: PathBuf = format!("{here_name}-{there_name}.prf").into();
+        let mut file = std::fs::File::create(&prf_path)?;
         writeln!(file, "# This was AUTOMATICALLY GENERATED - Do NOT edit!")?;
         writeln!(file)?;
-        writeln!(file, "{}", lines.join("\n"))?;
+        write!(file, "{}", config)?;
         writeln!(file)?;
         writeln!(file, "# End")?;
     }
@@ -80,7 +86,7 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-fn peer_configs<'a>(
+fn pair_configs<'a>(
     here_config: &'a Host,
     there_configs: &'a HashMap<String, Host>,
 ) -> impl Iterator<Item = (&'a String, &'a Host)> {
@@ -93,68 +99,64 @@ fn peer_configs<'a>(
     })
 }
 
-fn make_config(here: &Host, there: &Host, sets: &[&str]) -> Result<(String, Vec<String>)> {
-    let mut lines = Vec::new();
+fn config(
+    (here_name, here): (&str, &Host),
+    (there_name, there): (&str, &Host),
+    sets: &[&str],
+) -> Result<String> {
+    let mut config = String::new();
 
-    include(&mut lines, &[], sets)?;
+    include(&mut config, &[], sets)?;
 
-    lines.push("# Don't use domain when deriving archive names.".to_string());
-    lines.push(format!("clientHostName = {}", here.hostname));
-    lines.push("".to_string());
+    writeln!(config, "# Don't use domain when deriving archive names.")?;
+    writeln!(config, "clientHostName = {}", here.hostname)?;
+    writeln!(config)?;
 
-    lines.push(format!("root = {}", here.home.display()));
-    lines.push(format!(
+    writeln!(config, "root = {}", here.home.to_string_lossy())?;
+    writeln!(
+        config,
         "root = ssh://{}/{}",
         there.hostname,
-        there.home.display()
-    ));
-    lines.push("".to_string());
-    lines.push(format!(
-        "logfile = {}/.logs/unison.{}-{}.log",
-        here.home.display(),
-        here.hostname,
-        there.hostname
-    ));
+        there.home.to_string_lossy()
+    )?;
+    writeln!(config)?;
+    writeln!(
+        config,
+        "logfile = {}/.logs/unison.{here_name}-{there_name}.log",
+        here.home.to_string_lossy(),
+    )?;
 
-    let extra = here
-        .client
-        .iter()
-        .chain(there.server.iter())
-        .collect::<Vec<_>>();
-
+    let extra = chain!(&here.client, &there.server).collect::<Vec<_>>();
     if !extra.is_empty() {
-        lines.push("".to_string());
-        lines.push("# Extra configuration.".to_string());
-        lines.extend(extra.iter().map(|line| line.to_string()));
+        writeln!(config)?;
+        writeln!(config, "# Extra configuration.")?;
+        for line in extra {
+            writeln!(config, "{line}")?;
+        }
     }
 
-    Ok((format!("{}-{}", here.hostname, there.hostname), lines))
+    Ok(config)
 }
 
-fn include(lines: &mut Vec<String>, parents: &[&str], names: &[&str]) -> Result<()> {
+fn include(config: &mut String, parents: &[&str], names: &[&str]) -> Result<()> {
     for name in names {
         let path = PathBuf::from("sets").join(name);
-        let setlines = std::fs::read_to_string(path)?;
-        let setlines = setlines.trim().lines();
+        let set = std::fs::read_to_string(path)?;
 
-        let lineage = [*name]
-            .iter()
-            .copied()
-            .chain(parents.iter().copied())
-            .collect::<Vec<_>>();
-        lines.push(format!("### {}", lineage.join(", from ")));
+        let lineage = chain!([name], parents).copied().collect::<Vec<_>>();
+        writeln!(config, "### {}", lineage.join(", from "))?;
 
-        for line in setlines {
+        for line in set.trim().lines() {
             if line.starts_with("include") {
                 let names = line.split_whitespace().skip(1).collect::<Vec<_>>();
-                include(lines, &lineage, &names)?;
+                include(config, &lineage, &names)?;
             } else {
-                lines.push(line.to_string());
+                writeln!(config, "{line}")?;
             }
         }
 
-        lines.push(format!("### /{}", name));
-        lines.push("".to_string());
+        writeln!(config, "### /{name}")?;
+        writeln!(config)?;
     }
 
     Ok(())
